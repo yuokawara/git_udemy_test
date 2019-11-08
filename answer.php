2a31cc1a01d941e8721683c783fd01dad21db4e1
大河原悠 <solbonta1@gmail.com>
2019-11-08 10:06:21 +0900

    omikuji追加5

--- answer.php
+++ answer.php
@@ -4,31 +4,42 @@ $answer = $_POST['answer'];
 
 if ($question === $answer) {
     $result = "正解";
-}else{
+} else {
     $result = "不正解";
 }
 ?>
-
-<?php //第二問
-$question_2 = $_POST['question_2'];
-$answer_2 = $_POST['answer_2'];
-
-if ($question_2 === $answer_2) {
-    $result_2 = "正解";
-}else{
-    $result_2 = "不正解";
+<?php
+$q2 = $_POST["q2"];
+if ($q2 == "ばら") {
+    $result_2 = '正解';
+} else {
+    $result_2 = '不正解';
 }
 ?>
+<?php
+$q3 = $_POST["q3"];
+if ($q3 == "東京") {
+    $result_3 = "正解";
+} else {
+    $result_3 = "不正解";
+}
+?>
+
 <!DOCTYPE html>
+
 <head>
-<meta charset="utf-8">
-<title>クイズ結果</title>
+    <meta charset="utf-8">
+    <title>クイズ結果</title>
 </head>
+
 <body>
     <h2>結果は・・・</h2>
+    <h2>第一問</h2>
     <?php echo $result ?>
     <br>
     <h2>第二問</h2>
     <?php echo $result_2 ?>
-
+    <br>
+    <h2>第三問</h2>
+    <?php echo $result_3 ?>
 </body>
--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html>
+
 
 <head>
     <meta charset="utf-8">
     <title>クイズ</title>
     <?php
+    //第一問
     $title = "次のうち、北海道ではない地名はどれ？";
     $question = array();
     $question = array("苫小牧", "稚内", "恩納村", "北広島");
@@ -12,12 +13,13 @@
     shuffle($question);
 
     //第二問
-    $title_2 = "喋れる言語は？";
-    $question_2 = array();
-    $question_2 = array("英語", "タイ語", "スペイン語", "日本語");
-    $answer_2 = $question_2[3];
-
+    $title_2 = "薔薇はなんと読む？";
+    $q2 = array();
+    $q2 = array("きく", "ゆり", "ばら", "ひまわり");
     
+    //第三問
+    $title_3 = "2020年に開催されるのは、〇〇オリンピック";
+
 
 
     ?>
@@ -32,17 +34,19 @@
         <?php } ?>
         <input type="hidden" name="answer" value="<?php echo $answer ?>">
         <br>
-
-        <h2>第二問</h2>
-        <h2><?php echo $title_2 ?></h2>
-        <?php foreach ($question_2 as $value_2) { ?>
-            <input type="checkbox" name="question_2" value="<?php echo $value_2; ?>" /><?php echo $value_2; ?><br>
+    <h2>第二問</h2>
+    <h2><?php echo $title_2 ?></h2>
+        <?php foreach ($q2 as $value) { ?>
+            <input type="checkbox" name="q2" value="<?php echo $value; ?>" /><?php echo $value; ?><br>
         <?php } ?>
-        <input type="hidden" name="answer_2" value="<?php echo $answer_2 ?>">
         <br>
+    <h2>第三問</h2>
+    <h2><?php echo $title_3 ?></h2>
+        <textarea name="q3"></textarea><br>
 
-        <input type="submit" value="解答">
+
+        <br><input type="submit" value="解答">
     </form>
-</body>
 
-</html>
+
+</body>
