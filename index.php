7234e26466c196f478d856dca628ae4a52733b80
yuokawara <47330686+yuokawara@users.noreply.github.com>
2020-01-10 17:38:07 +0900

    Update index.php

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
     <h2>12345</h2>
     <p>git diff 変更</p>
     <p>git commit --amend</p>
+    <p>git merge練習</p>
     <h2>第一問</h2>
     <h2><?php echo $title ?></h2>
     <form method="POST" action="answer.php">
